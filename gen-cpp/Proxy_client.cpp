a23cb4c6f18356ba88bb86bbd9840e84600047e4
Ning Wang <glningwang@gmail.com>
2015-11-05 19:53:13 +0000

    Proxy client now takes filename as input. Needs more sample data.

--- gen-cpp/Proxy_client.cpp
+++ gen-cpp/Proxy_client.cpp
@@ -3,26 +3,35 @@
 #include <transport/TSocket.h>
 #include <transport/TBufferTransports.h>
 #include <protocol/TBinaryProtocol.h>
+#include <fstream>
 
 using namespace apache::thrift;
 using namespace apache::thrift::protocol;
 using namespace apache::thrift::transport;
 
 using namespace Proxy;
+using namespace std;
 
 int main(int argc, char **argv) {
     if (argc != 2)
-        std::cout << "Usage: ./Proxy_client url" << std::endl;
+        std::cout << "Usage: ./Proxy_client filename" << std::endl;
     boost::shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
     boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
     boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
 
+    ifstream file(argv[1]);
+    if (!file)
+        cout << "Cannot open file." << endl;
+
     ProxyClient client(protocol);
-    transport->open();
-    client.ping();
-    std::string response;
-    client.getURL(response, argv[1]);
-    std::cout << response << std::endl;
+    transport->open(); 
+    string url;
+    while (getline(file, url))
+    {
+        std::string response;
+        client.getURL(response, url.c_str());
+        std::cout << response << std::endl; 
+    }
     transport->close();
 
     return 0;
